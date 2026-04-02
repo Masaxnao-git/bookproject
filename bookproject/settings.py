c834c2aea67dd11de4dfac5aaa4e18b70edf85b4
masanaox-g <wind.and.adventurer@gmail.com>
2026-04-02 16:51:02 +0900

    second

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -1,4 +1,7 @@
+ALLOWED_HOSTS = ['*']
+
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,6 +74,14 @@ DATABASES = {
     }
 }
 
+if not DEBUG:
+    DATABASES = {
+        'default': dj_database_url.configc(
+            # Replace this value with your local database's connection string.
+            default='postgresql://postgres@localhost:5432/bookproject',
+            conn_max_age=600
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
