d1e5fe6a79738b70a752683b1a9724a645ff4506
masanaox-g <wind.and.adventurer@gmail.com>
2026-04-06 11:17:49 +0900

    first commit2

--- book/management/commands/superuser.py
+++ book/management/commands/superuser.py
@@ -6,9 +6,9 @@ User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username='your_name').exists():
+        if not User.objects.filter(username='masaxnao').exists():
             User.objects.create_superuser(
-                username='your_name',
+                username='masaxnao',
                 email='',
-                password='your_password'
+                password='bookproject_test'
             )
